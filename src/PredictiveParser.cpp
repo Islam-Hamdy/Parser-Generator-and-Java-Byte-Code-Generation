d8ef5c23c6e81aa65c1014f21fb9888123be8ea1
Islam-Hamdy <islam.hamdy48@gmail.com>
2013-05-16 04:16:02 +0200

    nullables

--- src/PredictiveParser.cpp
+++ src/PredictiveParser.cpp
@@ -7,6 +7,8 @@
 
 #include "PredictiveParser.h"
 #include <cstdio>
+#include <stdio.h>
+#include <string.h>
 #include <algorithm>
 #include <cstring>
 
@@ -14,5 +16,33 @@ using namespace std;
 
 PredictiveParser::PredictiveParser(Parser* par) {
 	parser = par;
+	visited = new bool[parser->rev_m.size()];
+	initializeNullables();
 }
 
+void PredictiveParser::initializeNullables() {
+	map<int, pair<string, bool> >::iterator miter = parser->rev_m.begin();
+	while (miter != parser->rev_m.end()) {
+		nullables[(*miter).first] = isNullable((*miter).first);
+		miter++;
+	}
+}
+
+bool PredictiveParser::isNullable(int symbolId) {
+	if (!(parser->rev_m[symbolId].first.compare("\L"))) // epsilon match
+		return true;
+
+	vector<vector<int> > RHS = parser->g[symbolId];
+	bool answer = false;
+	for (int i = 0; !answer && i < RHS.size(); i++) {
+		vector<int> cur = RHS[i];
+		bool status = true;
+		for (int j = 0; status && j < cur.size(); j++)
+			if (!visited[cur[j]]) {
+				visited[cur[j]] = true;
+				status &= isNullable(cur[j]);
+			}
+		answer |= status;
+	}
+	return answer;
+}
--- src/PredictiveParser.h
+++ src/PredictiveParser.h
@@ -18,10 +18,12 @@ public:
 	Parser * parser;
 	map<int, bool> nullables; // if symbol with id x is nullable or not
 	map<int, vector<int> > firstSets; //first set vectors
+	bool* visited;
 	vector<vector<int> > predictive_table;
 
 	PredictiveParser(Parser* par);
 	bool isNullable(int symbolId);
+	void initializeNullables();
 	void constructTable();
 
 };
